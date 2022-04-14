cacdb4cb8e17a4e61e4a1d279d2bab6c10f6d229
geethugmail <42536446+geethugmail@users.noreply.github.com>
2022-04-14 18:35:12 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 ON_DOCKER = 'IS_DOCKER' in os.environ
 DEBUG = False if ON_DOCKER else True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
